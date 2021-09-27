6c2abb0a0697bb9d94df46b96517eeef512ea97d
Oliver-makes-code <kookooloopdaking@gmail.com>
2021-09-26 20:12:52 -0500

    Add abstract webhookManager class for later use

--- src/webhookManager.ts
+++ src/webhookManager.ts
@@ -1,5 +1,26 @@
-import { Webhook } from "discord.js";
+import { ReactionUserManager, Webhook } from "discord.js";
 
 export interface webhookStorage {
     [key: string]: Webhook;
+}
+
+export class webhookManager {
+    webhooks: webhookStorage = {};
+    public constructor() {
+
+    }
+    public put(key: string, value: Webhook) {
+        this.webhooks[key] = value;
+    }
+    public has(key: string): boolean {
+        return !!this.webhooks[key];
+    }
+    public get(key: string): Webhook {
+        if (!this.has(key)) return null;
+        return this.webhooks[key];
+    }
+    public remove(key: string) {
+        if (!this.has(key)) return;
+        this.webhooks[key] = undefined;
+    }
 }
