657119e0efca6062c1e966ae08da54deccad7e19
Octal <oliver@olivermakesco.de>
2022-04-09 15:29:20 -0500

    Set up importer classes

---
+++ src/main/kotlin/io/github/proxyfox/importer/PluralKitImporter.kt
@@ -0,0 +1,16 @@
+package io.github.proxyfox.importer
+
+/**
+ * [Importer] to import a JSON with a PluralKit format
+ *
+ * @author Oliver
+ * */
+class PluralKitImporter : Importer {
+    override suspend fun import(map: Map<String, *>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun finalizeImport() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/io/github/proxyfox/importer/TupperBoxImporter.kt
@@ -0,0 +1,16 @@
+package io.github.proxyfox.importer
+
+/**
+ * [Importer] to import a JSON with a TupperBox format
+ *
+ * @author Oliver
+ * */
+class TupperBoxImporter : Importer {
+    override suspend fun import(map: Map<String, *>) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun finalizeImport() {
+        TODO("Not yet implemented")
+    }
+}
