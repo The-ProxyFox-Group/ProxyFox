e215dfc1458f44bf63c01d194f25f5ad7f8108b4
Oliver-makes-code <kookooloopdaking@gmail.com>
2022-04-08 20:36:28 -0500

    Finish mapping member commands

--- src/main/kotlin/command/MemberCommands.kt
+++ src/main/kotlin/command/MemberCommands.kt
@@ -16,6 +16,62 @@ object MemberCommands {
         //TODO: not implemented
         return 0
     }
+    private fun changeServerName(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeAvatar(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeAvatarLinked(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeServerAvatar(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeServerAvatarLinked(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun addProxy(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun removeProxy(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeMemberDescription(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeMemberPronouns(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeMemberColor(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun changeMemberBirthday(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun deleteMember(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun accessMember(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
+    private fun createMember(ctx: CommandContext<CommandSource>): Int {
+        //TODO: not implemented
+        return 0
+    }
 
     suspend fun register() {
         commands(arrayOf("member","m")) {
@@ -26,8 +82,8 @@ object MemberCommands {
                         executes(MemberCommands::changeName)
                     }
                 }
-                literal("name",name)
-                literal("rename",name)
+                literal("name", name)
+                literal("rename", name)
 
                 // Change member display name
                 val displayname: Node = {
@@ -35,13 +91,99 @@ object MemberCommands {
                         executes(MemberCommands::changeDisplayName)
                     }
                 }
-                literal("displayname",displayname)
-                literal("dn",displayname)
-                literal("nickname",displayname)
-                literal("nick",displayname)
+                literal("displayname", displayname)
+                literal("dn", displayname)
+                literal("nickname", displayname)
+                literal("nick", displayname)
 
+                // Change member server nickname
+                literal("servernick") {
+                    argument("name",StringArgumentType.greedyString()) {
+                        executes(MemberCommands::changeServerName)
+                    }
+                }
 
+                // Change member avatar
+                val avatar: Node = {
+                    argument("link", StringArgumentType.greedyString()) {
+                        executes(MemberCommands::changeAvatarLinked)
+                    }
+                    executes(MemberCommands::changeAvatar)
+                }
+                literal("avatar", avatar)
+                literal("pfp", avatar)
+
+                // Change member server avatar
+                val serveravatar: Node = {
+                    argument("link", StringArgumentType.greedyString()) {
+                        executes(MemberCommands::changeServerAvatarLinked)
+                    }
+                    executes(MemberCommands::changeServerAvatar)
+                }
+                literal("serveravatar", serveravatar)
+                literal("serverpfp", serveravatar)
+
+                // Edit member proxy
+                literal("proxy") {
+                    literal("add") {
+                        argument("proxy",StringArgumentType.greedyString()) {
+                            executes(MemberCommands::addProxy)
+                        }
+                    }
+                    literal("remove") {
+                        argument("proxy",StringArgumentType.greedyString()) {
+                            executes(MemberCommands::removeProxy)
+                        }
+                    }
+                    argument("proxy",StringArgumentType.greedyString()) {
+                        executes(MemberCommands::addProxy)
+                    }
+                }
+
+                // Change member description
+                literal("description") {
+                    literal("description") {
+                        executes(MemberCommands::changeMemberDescription)
+                    }
+                }
+
+                // Change member pronouns
+                literal("pronouns") {
+                    literal("pronouns") {
+                        executes(MemberCommands::changeMemberPronouns)
+                    }
+                }
+
+                // Change member color
+                literal("color") {
+                    literal("color") {
+                        executes(MemberCommands::changeMemberColor)
+                    }
+                }
+
+                // Change member birthday
+                literal("birthday") {
+                    literal("birthday") {
+                        executes(MemberCommands::changeMemberBirthday)
+                    }
+                }
+
+                // Delete member
+                literal("delete") {
+                    executes(MemberCommands::deleteMember)
+                }
+
+                // Access member
+                executes(MemberCommands::accessMember)
             }
+            val create: Node = {
+                argument("name", StringArgumentType.greedyString()) {
+                    executes(MemberCommands::createMember)
+                }
+            }
+            literal("new",create)
+            literal("add",create)
+            literal("create",create)
         }
     }
 }
