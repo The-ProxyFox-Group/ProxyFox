33875d3463f3dbd7847117462ccbb0d63e06d0b2
Octal <oliver@olivermakesco.de>
2022-04-10 15:29:32 -0500

    Remove unused imports

--- src/main/kotlin/io/github/proxyfox/api/RestApi.kt
+++ src/main/kotlin/io/github/proxyfox/api/RestApi.kt
@@ -1,18 +1,17 @@
 package io.github.proxyfox.api
 
 import io.github.proxyfox.printStep
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.slf4j.LoggerFactory
-import kotlin.concurrent.thread
 
 private val logger = LoggerFactory.getLogger("REST API")
 
 object RestApi {
+    /**
+     * @author Oliver
+     * */
     suspend fun start() {
         printStep("Start REST API server", 1)
         embeddedServer(Netty, 8080) {
