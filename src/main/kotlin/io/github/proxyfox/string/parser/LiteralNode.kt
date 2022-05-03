dfc88705b71de0f7c0f0bafe0ca1def5ecc2a184
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-05-03 18:50:28 -0500

    Add executor

--- src/main/kotlin/io/github/proxyfox/string/parser/LiteralNode.kt
+++ src/main/kotlin/io/github/proxyfox/string/parser/LiteralNode.kt
@@ -1,22 +1,20 @@
 package io.github.proxyfox.string.parser
 
-class LiteralNode(val literal: String) : Node {
+class LiteralNode(val literal: String, val executor: suspend (MessageHolder) -> String) : Node {
     val nodes: ArrayList<Node> = ArrayList()
 
-    override fun parse(string: String, index: Int): Int {
+    override suspend fun parse(string: String, index: Int, holder: MessageHolder): Int {
         if (string.substring(index, literal.length).lowercase() == literal.lowercase()) {
             return index + literal.length
         }
         return index
     }
 
-    override fun getSubNodes(): List<Node> = nodes
+    override suspend fun getSubNodes(): List<Node> = nodes
 
-    override fun addSubNode(node: Node) {
+    override suspend fun addSubNode(node: Node) {
         nodes.add(node)
     }
 
-    override fun execute(holder: MessageHolder) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun execute(holder: MessageHolder) = executor(holder)
 }
--- src/main/kotlin/io/github/proxyfox/string/parser/StringLexer.kt
+++ src/main/kotlin/io/github/proxyfox/string/parser/StringLexer.kt
@@ -2,7 +2,7 @@ package io.github.proxyfox.string.parser
 
 val nodes: List<Node> = ArrayList()
 
-fun parseString(input: String) {
+suspend fun parseString(input: String) {
     var index = 0
 
 }
