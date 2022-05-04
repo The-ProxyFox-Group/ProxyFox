120baa5a748ae54a91b27ae1252c774543872eb2
Octal <oliver@olivermakesco.de>
2022-05-03 22:12:24 -0500

    Clean up unused code

--- src/main/kotlin/io/github/proxyfox/command/CommandSource.kt
+++
@@ -1,9 +0,0 @@
-package io.github.proxyfox.command
-
-import dev.kord.core.entity.Message
-
-/**
- * Stores important data relating to commands
- * @author Oliver
- * */
-class CommandSource(val message: Message)
--- src/main/kotlin/io/github/proxyfox/command/Commands.kt
+++ src/main/kotlin/io/github/proxyfox/command/Commands.kt
@@ -1,6 +1,5 @@
 package io.github.proxyfox.command
 
-import com.mojang.brigadier.CommandDispatcher
 import io.github.proxyfox.printStep
 import io.github.proxyfox.string.node.Node
 
@@ -8,8 +7,6 @@ import io.github.proxyfox.string.node.Node
  * General utilities relating to commands
  * @author Oliver
  * */
-
-val dispatcher = CommandDispatcher<CommandSource>()
 
 typealias CommandNode = Node.() -> Unit
 
