061adadbbe43c2ee3f4573e943df56a95f94c868
Octal <oliver@olivermakesco.de>
2022-05-07 08:33:20 -0500

    Completely crab brigadier

--- src/main/kotlin/io/github/proxyfox/terminal/TerminalCommands.kt
+++ src/main/kotlin/io/github/proxyfox/terminal/TerminalCommands.kt
@@ -1,9 +1,6 @@
 package io.github.proxyfox.terminal
 
-import com.mojang.brigadier.CommandDispatcher
-import dev.steyn.brigadierkt.command
 import io.github.proxyfox.printStep
-import io.github.proxyfox.runAsync
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 
@@ -11,35 +8,20 @@ import kotlin.system.exitProcess
  * Terminal related functions and variables
  * @author Oliver
  * */
-val terminalDispatcher = CommandDispatcher<TerminalCommandSource>()
 
 object TerminalCommands {
     suspend fun start() {
         printStep("Start reading console input", 1)
-        register()
         startThread()
     }
 
     suspend fun startThread() {
         printStep("Launching thread", 2)
         thread {
-            runAsync {
-                while (true) {
-                    val input = readln()
-                    terminalDispatcher.execute(input, TerminalCommandSource())
-                }
-            }
-        }
-    }
-
-    suspend fun register() {
-        printStep("Registering commands", 2)
-        terminalDispatcher.command("stop") {
-            executes {
-                exitProcess(0)
+            while (true) {
+                val input = readln()
+                if (input.lowercase() == "exit") exitProcess(0)
             }
         }
     }
 }
-
-class TerminalCommandSource
