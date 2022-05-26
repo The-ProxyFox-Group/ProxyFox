c1c9cb7da09d624628ca0b698d2b6a1fc9c9429a
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-05-26 18:28:08 -0500

    Deprecate PostgreSQL driver as unmaintained for time being

--- src/main/kotlin/io/github/proxyfox/database/PostgresDatabase.kt
+++ src/main/kotlin/io/github/proxyfox/database/PostgresDatabase.kt
@@ -27,6 +27,7 @@ import java.util.*
  * @author KJP12
  * @since ${version}
  **/
+@Deprecated("Will be unmaintained until Hachmitsu is usable. For now, use the JSON or Mongo drivers.")
 class PostgresDatabase(val driver: Driver) : Database {
     private val logger = LoggerFactory.getLogger(PostgresDatabase::class.java)
 
