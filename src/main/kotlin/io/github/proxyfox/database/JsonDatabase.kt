74d3e094f4e8be37da771115121f762aeb20bf84
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-05-28 16:38:31 -0500

    Update expected structure

--- src/main/kotlin/io/github/proxyfox/database/JsonDatabase.kt
+++ src/main/kotlin/io/github/proxyfox/database/JsonDatabase.kt
@@ -16,81 +16,115 @@ import io.github.proxyfox.database.records.system.SystemSwitchRecord
  *
  * ## Expected File Structure
  * ```json5
- * {
- *   "discordId": {
- *     // Will not be changed unless a backwards-incompatible change is required.
- *     "version": 1,
- *     // System ID (not to be confused with the Discord ID)
- *     "id": "string.of(systemId)",
- *     "name": "string",
- *     "description": "string",
- *     "tag": "string",
- *     // Map to `avatarUrl`
- *     "avatar_url": "url",
- *     "timezone": "string",
- *     "members": [
- *       {
- *         // Member ID
- *         "id": "string",
- *         "name": "string",
- *         // Map to `displayName`
- *         "display_name": "string",
- *         "description": "string",
- *         "birthday": "string",
- *         "pronouns": "string",
- *         // Map to integer or -1
- *         "color": "#hex",
- *         // Map to `avatarUrl`
- *         "avatar_url": "url",
- *         // Fairly complex indexing required
- *         "proxy_tags": [
- *           {
- *             "prefix": "string",
- *             "suffix": "string"
- *           },
- *           ...
- *         ],
- *         // Map to `keepProxy`
- *         "keep_proxy": false,
- *         // Map to `messageCount`
- *         "message_count": 0,
- *         "created": "ISO 8601",
- *
- *         // == Map the following to guild settings ==
- *         "server_avatar": {
- *           "guildId": "url"
- *         },
- *         "server_nick": {
- *           "guildId": "string"
- *         }
- *         "server_proxy": {
- *           "guildId": false
- *         }
+ *   {
+ *       "systems": {
+ *           "sysid": {
+ *               // here for redundancy purposes
+ *               "id": "sysid",
+ *               // list of user snowflakes of users that own the system (have it set as 'system' in their user object
+ *               "accounts": [],
+ *               // name of the system
+ *               "name": "name",
+ *               // description that the user set
+ *               "description": "description",
+ *               // tag that shows when proxying to identify systems
+ *               "tag": "tag",
+ *               // default avatar for when a member doesn't have an avatar set
+ *               "avatarUrl": "avatar url",
+ *               // timestamp of creation
+ *               "timestamp": "timestamp",
+ *               // the member to autoproxy
+ *               "auto", "memid",
+ *               // the autoproxy type
+ *               "autoType": "autoproxy type",
+ *               "members": {
+ *                   "memid": {
+ *                       // here for redundancy
+ *                       "id": "memid",
+ *                       // name of the member, can be used to index
+ *                       "name": "member name",
+ *                       // display namme of the member, shown when proxying
+ *                       "displayName": "display name",
+ *                       // description that the member set
+ *                       "description": "member description",
+ *                       // birth date of the member
+ *                       "birthday": "member birthday"
+ *                       // age of the member
+ *                       "age": "0",
+ *                       // role of the member
+ *                       "role": "role",
+ *                       // pronouns of the member
+ *                       "pronouns": "pronouns",
+ *                       // member's avatar
+ *                       "avatarUrl": "avatar url",
+ *                       // whether to keep proxy tags
+ *                       "keepProxy": false,
+ *                       // the amount of messages sent by the member
+ *                       "messageCount": 0,
+ *                       // timestamp of when the member was created
+ *                       "timestamp": "timestamp",
+ *                       // display name per server
+ *                       "serverDisplayName": {
+ *                           "server snowflake": "display name"
+ *                       },
+ *                       // avatar URL per server
+ *                       "serverAvatarUrl": {
+ *                           "server snowflake": "avatar url"
+ *                       },
+ *                       // proxy per server
+ *                       "serverProxy": {
+ *                           "server snowflake": false
+ *                       }
+ *                   }
+ *               },
+ *               // proxy per server
+ *               "serverProxy": {
+ *                   "server snowflake": false
+ *               },
+ *               // array of proxy tags (each prefix/suffix pair will be unique)
+ *               "proxyTags": [
+ *                   {
+ *                       "prefix": "prefix"
+ *                       "suffix": "suffix",
+ *                       "member": "memid"
+ *                   }
+ *               ],
+ *               // array of switches
+ *               "switches": {
+ *                   "switch": {
+ *                       // here for redundancy
+ *                       "id": "switch"
+ *                       // timestamp of the switch
+ *                       "timestamp": "timestamp",
+ *                       // array of fronting members
+ *                       "members": [],
+ *                   }
+ *               }
+ *           }
  *       },
- *       ...
- *     ],
- *     // Map to `autoProxy`
- *     "auto": "memberId"
- *     // Map to `autoProxyEnabled` via converting to AutoProxyMode.
- *     "auto_bool", false,
- *     "switches": [
- *       {
- *         "timestamp": "ISO 8601",
- *         "members": [ "memberId", ... ],
- *         "id": "switchId"
+ *       "users": {
+ *           "user snowflake": {
+ *               "system": "sysid",
+ *               // appended to when systems grant trust
+ *               "trusted": {
+ *                   "sysid": "LEVEL"
+ *               }
+ *           }
  *       },
- *       ...
- *     ],
- *
- *     // == Map the following to guild settings ==
- *     "server_proxy": {
- *       "guildId": false
- *     }
+ *       "servers": {
+ *           "server snowflake": {
+ *               "channels": {
+ *                   // false if disabled
+ *                   "channel snowflake": true
+ *               },
+ *               // role to enable proxying
+ *               "role": "role snowflake"
+ *           }
+ *       }
  *   }
- * }
  * ```
  *
- * @author KJP12
+ * @author KJP12, Ram
  * @since ${version}
  **/
 class JsonDatabase : Database() {
