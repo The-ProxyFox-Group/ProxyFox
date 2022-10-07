8c3e3e78058be9685f7b481422afd4ebcf4d0bda
oliver <kookooloopdaking@gmail.com>
2022-10-06 21:25:07 -0500

    Extract git hash

--- modules/common/build.gradle.kts
+++ modules/common/build.gradle.kts
@@ -6,6 +6,9 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
+import java.io.ByteArrayOutputStream
+import java.nio.charset.*
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
 }
@@ -13,4 +16,26 @@ plugins {
 dependencies {
     api(libs.bundles.base)
     api(kotlin("stdlib"))
+}
+
+tasks.withType<ProcessResources> {
+    val hash = getCommitHash()
+    inputs.property("hash", hash)
+    filesMatching("commit_hash.txt") {
+        expand("hash" to hash)
+    }
+}
+
+tasks.build {
+    dependsOn(ProcessResources::class)
+}
+
+fun getCommitHash(): String? {
+    val stdout = ByteArrayOutputStream()
+    exec {
+        commandLine("git", "log", "-n", "1", "--pretty=format:\"%h\"", "--encoding=UTF-8")
+        standardOutput = stdout
+    }
+    val str = stdout.toString(Charset.defaultCharset())
+    return str.substring(1, str.length-1)
 }
