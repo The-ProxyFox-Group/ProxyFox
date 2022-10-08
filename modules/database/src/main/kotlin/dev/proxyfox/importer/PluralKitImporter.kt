4bba93656424da0467c468b5602210c7af2bb118
Ampflower <ampflower@outlook.com>
2022-10-07 19:29:46 -0500

    Refactor PluralKit importer and exporter

--- modules/database/src/main/kotlin/dev/proxyfox/importer/PluralKitImporter.kt
+++ modules/database/src/main/kotlin/dev/proxyfox/importer/PluralKitImporter.kt
@@ -10,52 +10,135 @@ package dev.proxyfox.importer
 
 import com.google.gson.JsonObject
 import dev.proxyfox.common.toColor
-import dev.proxyfox.database.Database
-import dev.proxyfox.database.gson
+import dev.proxyfox.database.*
 import dev.proxyfox.database.records.member.MemberProxyTagRecord
 import dev.proxyfox.database.records.member.MemberRecord
 import dev.proxyfox.database.records.system.SystemRecord
-import dev.proxyfox.database.sanitise
-import dev.proxyfox.database.validate
+import dev.proxyfox.types.PkMember
 import dev.proxyfox.types.PkSystem
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 /**
  * [Importer] to import a JSON with a PluralKit format
  *
+ * @param directAllocation Whether to directly allocate IDs where possible.
+ * @param ignoreUnfinished Whether to ignore unfinished features from PFv1 that were inadvertently included via imports.
+ *                         Includes ignoring groups, subsystems and privacy settings.
  * @author Oliver
  * */
-class PluralKitImporter : Importer {
+open class PluralKitImporter protected constructor(
+    private val directAllocation: Boolean,
+    private val ignoreUnfinished: Boolean,
+) : Importer {
     private lateinit var system: SystemRecord
     private var members: List<MemberRecord> = ArrayList()
     private var proxies: HashMap<MemberRecord, List<MemberProxyTagRecord>> = HashMap()
     private var createdMembers = 0
     private var updatedMembers = 0
 
+    constructor() : this(false, false)
+
     override suspend fun import(database: Database, json: JsonObject, userId: ULong) {
         val pkSystem = gson.fromJson(json, PkSystem::class.java)
-        system = database.getOrCreateSystem(userId)
+        system = database.getOrCreateSystem(userId, id = if (directAllocation) pkSystem.id else null)
         system.name = pkSystem.name.sanitise() ?: system.name
         system.description = pkSystem.description.sanitise() ?: system.description
         system.tag = pkSystem.tag.sanitise() ?: system.tag
         system.avatarUrl = pkSystem.avatar_url.sanitise() ?: system.avatarUrl
-        if (pkSystem.members != null)
-            for (pkMember in pkSystem.members!!) {
+
+        pkSystem.pronouns.sanitise()?.let { system.pronouns = it }
+        try {
+            pkSystem.color?.validate("system/color")?.toColor()?.let { system.color = it }
+        } catch (nfe: NumberFormatException) {
+            throw ImporterException("Invalid colour given for system: ${pkSystem.color}", nfe)
+        }
+        pkSystem.config?.timezone.sanitise()?.let { system.timezone = it }
+
+        if (directAllocation) {
+            pkSystem.proxyfox?.run {
+                autoProxy?.let { system.autoProxy = it }
+                autoType?.let { system.autoType = it }
+                trust?.let(system.trust::putAll)
+            }
+            pkSystem.accounts?.let(system.users::addAll)
+            pkSystem.created.tryParseOffsetTimestamp()?.let { system.timestamp = it }
+        }
+
+        if (pkSystem.members != null) {
+            val birthdays = findBirthdays(pkSystem.members)
+
+            for (pkMember in pkSystem.members) {
                 val member = database.fetchMemberFromSystemAndName(system.id, pkMember.name.validate("members/name"))?.apply { updatedMembers++ }
-                    ?: database.getOrCreateMember(system.id, pkMember.name.validate("members/name"))?.apply { createdMembers++ }
+                    ?: database.getOrCreateMember(system.id, pkMember.name.validate("members/name"), id = if (directAllocation) pkMember.id else null)?.apply { createdMembers++ }
                     ?: throw ImporterException("Database didn't create member")
                 member.displayName = pkMember.display_name.sanitise() ?: member.displayName
                 member.avatarUrl = pkMember.avatar_url.sanitise() ?: member.avatarUrl
                 member.description = pkMember.description.sanitise() ?: member.description
                 member.pronouns = pkMember.pronouns.sanitise() ?: member.pronouns
-                member.color = pkMember.color?.validate("members[${pkMember.id} (${pkMember.name})]/color")?.toColor() ?: member.color
+                try {
+                    member.color = pkMember.color?.validate("members[${pkMember.id} (${pkMember.name})]/color")?.toColor() ?: member.color
+                } catch (nfe: NumberFormatException) {
+                    throw ImporterException("Invalid colour given for member ${pkMember.id} (${pkMember.name}): ${pkSystem.color}", nfe)
+                }
                 member.keepProxy = pkMember.keep_proxy ?: member.keepProxy
-                member.messageCount = pkMember.message_count ?: member.messageCount
+                member.messageCount = pkMember.message_count?.toULong() ?: member.messageCount
+                birthdays[pkMember]?.let { member.birthday = it }
+
+                pkMember.proxyfox?.let { proxyfox ->
+                    proxyfox.age?.let { member.age = it }
+                    proxyfox.role?.let { member.role = it }
+                }
+
                 pkMember.proxy_tags?.forEach { pkProxy ->
                     database.createProxyTag(system.id, member.id, pkProxy.prefix, pkProxy.suffix)
                 }
+                if (directAllocation) {
+                    pkMember.created.tryParseOffsetTimestamp()?.let { member.timestamp = it }
+                }
                 database.updateMember(member)
             }
+        }
         database.updateSystem(system)
+    }
+
+    private fun findBirthdays(members: Collection<PkMember>): Map<PkMember, LocalDate> {
+        val preferMonthDay = shouldPreferMonthDay(system.timezone)
+        val ambiguousFormat = if (preferMonthDay) MMDDuuuu else DDMMuuuu
+
+        val birthdays = HashMap<PkMember, Pair<LocalDate, DateTimeFormatter>>()
+        val ambiguousBirthdays = HashSet<PkMember>()
+
+        // Parse out birthdays ahead of time
+        for (pkMember in members) {
+            tryParseLocalDate(pkMember.birthday, preferMonthDay)?.let {
+                birthdays[pkMember] = it
+                if (it.second == ambiguousFormat && it.first.dayOfMonth <= 12) {
+                    ambiguousBirthdays.add(pkMember)
+                }
+            }
+        }
+
+        // If there's any ambiguous birthdays, figure out the expected format.
+        if (ambiguousBirthdays.isNotEmpty()) {
+            val otherFormat = if (preferMonthDay) DDMMuuuu else MMDDuuuu
+            var expectedCount = 0
+            var otherCount = 0
+            for ((pkMember, pair) in birthdays) {
+                if (!ambiguousBirthdays.contains(pkMember)) {
+                    if (pair.second == ambiguousFormat) expectedCount++
+                    if (pair.second == otherFormat) otherCount++
+                }
+            }
+            if (otherCount > expectedCount) {
+                for (pkMember in ambiguousBirthdays) {
+                    // Not null assertion as it was already parsed successfully once.
+                    birthdays[pkMember] = tryParseLocalDate(pkMember.birthday, !preferMonthDay)!!
+                }
+            }
+        }
+
+        return birthdays.mapValues { it.value.first }
     }
 
     // Getters:
--- modules/database/src/main/kotlin/dev/proxyfox/types/PkTypes.kt
+++ modules/database/src/main/kotlin/dev/proxyfox/types/PkTypes.kt
@@ -8,32 +8,241 @@
 
 package dev.proxyfox.types
 
-class PkSystem {
-    var id: String? = null
-    var name: String? = null
-    var description: String? = null
-    var tag: String? = null
-    var avatar_url: String? = null
-    var members: Array<PkMember>? = arrayOf()
+import dev.proxyfox.common.fromColorForExport
+import dev.proxyfox.database.records.member.MemberProxyTagRecord
+import dev.proxyfox.database.records.member.MemberRecord
+import dev.proxyfox.database.records.misc.AutoProxyMode
+import dev.proxyfox.database.records.misc.TrustLevel
+import dev.proxyfox.database.records.system.SystemRecord
+import dev.proxyfox.gson.NullValueProcessor
+import dev.proxyfox.gson.UnexpectedValueProcessor
+import java.time.OffsetDateTime
+
+@JvmRecord
+data class PkSystem(
+    val id: String? = null,
+    val name: String? = null,
+    val description: String? = null,
+    val tag: String? = null,
+    val pronouns: String? = null,
+    val avatar_url: String? = null,
+    val banner: String? = null,
+    val color: String? = null,
+    val created: String? = null,
+    val webhook_url: String? = null,
+
+    val privacy: PkSystemPrivacy? = null,
+    val config: PkConfig? = null,
+
+    val accounts: List<ULong>? = null,
+    val members: List<PkMember>? = null,
+    val groups: List<PkGroup>? = null,
+
+    // ProxyFox-specific extensions.
+    // PluralKit and TupperBox should ignore these.
+    val proxyfox: PfSystemExtension? = null,
 
     // Required for PK to accept the export.
-    var switches: Array<Any> = arrayOf()
+    // ProxyFox however will accept any export that vaguely matches PK's.
+    val switches: List<Any>? = emptyList(),
+) {
+    constructor(
+        record: SystemRecord,
+        members: List<PkMember>? = null,
+        groups: List<PkGroup>? = null
+    ) : this(
+        id = record.id,
+        accounts = record.users,
+        name = record.name,
+        description = record.description,
+        tag = record.tag,
+        pronouns = record.pronouns,
+        color = record.color.fromColorForExport(),
+        avatar_url = record.avatarUrl,
+        created = record.timestamp.toString(),
+        config = PkConfig(
+            timezone = record.timezone,
+        ),
+        members = members,
+        groups = groups,
+        proxyfox = PfSystemExtension(
+            trust = record.trust,
+            autoType = record.autoType,
+            autoProxy = record.autoProxy,
+        )
+    )
 }
 
-class PkMember {
-    var id: String? = null
-    var name: String = ""
-    var display_name: String? = null
-    var description: String? = null
-    var pronouns: String? = null
-    var color: String? = null
-    var keep_proxy: Boolean? = false
-    var message_count: ULong? = 0UL
-    var proxy_tags: Array<PkProxy>? = arrayOf()
-    var avatar_url: String? = null
+@JvmRecord
+data class PkMember(
+    val id: String? = null,
+    val name: String? = null,
+    val display_name: String? = null,
+    val description: String? = null,
+    val pronouns: String? = null,
+    val color: String? = null,
+    val keep_proxy: Boolean? = false,
+    val message_count: Long? = 0L,
+
+    // Note: Exporting is *strictly* ISO_LOCAL_DATE.
+    // Importing will be much more lenient due to PFv1 not enforcing proper constraints on birthdays.
+    val birthday: String? = null,
+
+    // Note: Exporting is *strictly* ISO_OFFSET_DATE_TIME.
+    // Importing will be much more lenient due to PFv1 not enforcing proper constraints on imports.
+    // PF will ignore this unless this is a direct allocation.
+    val created: String? = null,
+
+    // The following two will need to be sanitised to valid HTTP/HTTPS URIs.
+    val avatar_url: String? = null,
+    val banner: String? = null,
+
+    val proxy_tags: List<PkProxy>? = emptyList(),
+
+    // Some data structures from here will need to be flattened in.
+    val privacy: PkMemberPrivacy? = null,
+
+    // ProxyFox-specific extensions.
+    // PluralKit and TupperBox should ignore these.
+    val proxyfox: PfMemberExtension? = null,
+) {
+    constructor(record: MemberRecord, proxyTags: List<PkProxy>?) : this(
+        id = record.id,
+        name = record.name,
+        display_name = record.displayName,
+        description = record.description,
+        pronouns = record.pronouns,
+        color = record.color.fromColorForExport(),
+        keep_proxy = record.keepProxy,
+        message_count = record.messageCount.toLong(),
+        birthday = record.birthday?.toString(),
+        created = record.timestamp.toString(),
+        proxy_tags = proxyTags,
+        avatar_url = record.avatarUrl,
+        proxyfox = if (record.birthday != null || record.age != null || record.role != null) {
+            PfMemberExtension(
+                age = record.age,
+                role = record.role,
+            )
+        } else null
+    )
 }
 
-class PkProxy {
-    var prefix: String? = null
-    var suffix: String? = null
+@JvmRecord
+data class PkGroup(
+    val id: String? = null,
+    val name: String? = null,
+    val display_name: String? = null,
+    val description: String? = null,
+    val icon: String? = null,
+    val banner: String? = null,
+    val color: String? = null,
+    val created: OffsetDateTime? = null,
+    val members: List<String>? = null,
+
+    val privacy: PkGroupPrivacy? = null,
+)
+
+@JvmRecord
+data class PkProxy(
+    val prefix: String?,
+    val suffix: String?
+) {
+    constructor(record: MemberProxyTagRecord) : this(
+        prefix = record.prefix,
+        suffix = record.suffix
+    )
+}
+
+@UnexpectedValueProcessor<Any?>(NullValueProcessor::class)
+@JvmRecord
+data class PkSystemPrivacy(
+    val description_privacy: PkPrivacyEnum?,
+    val pronoun_privacy: PkPrivacyEnum?,
+    val member_list_privacy: PkPrivacyEnum?,
+    val group_list_privacy: PkPrivacyEnum?,
+    val front_privacy: PkPrivacyEnum?,
+    val front_history_privacy: PkPrivacyEnum?,
+) {
+    constructor(privacy: PkPrivacyEnum) : this(
+        description_privacy = privacy,
+        pronoun_privacy = privacy,
+        member_list_privacy = privacy,
+        group_list_privacy = privacy,
+        front_privacy = privacy,
+        front_history_privacy = privacy,
+    )
+}
+
+@UnexpectedValueProcessor<Any?>(NullValueProcessor::class)
+@JvmRecord
+data class PkMemberPrivacy(
+    val visibility: PkPrivacyEnum?,
+    val name_privacy: PkPrivacyEnum?,
+    val description_privacy: PkPrivacyEnum?,
+    val birthday_privacy: PkPrivacyEnum?,
+    val pronoun_privacy: PkPrivacyEnum?,
+    val avatar_privacy: PkPrivacyEnum?,
+    val metadata_privacy: PkPrivacyEnum?,
+) {
+    constructor(privacy: PkPrivacyEnum) : this(
+        visibility = privacy,
+        name_privacy = privacy,
+        description_privacy = privacy,
+        birthday_privacy = privacy,
+        pronoun_privacy = privacy,
+        avatar_privacy = privacy,
+        metadata_privacy = privacy,
+    )
+}
+
+@UnexpectedValueProcessor<Any?>(NullValueProcessor::class)
+@JvmRecord
+data class PkGroupPrivacy(
+    val description_privacy: PkPrivacyEnum?,
+    val pronoun_privacy: PkPrivacyEnum?,
+    val member_list_privacy: PkPrivacyEnum?,
+    val group_list_privacy: PkPrivacyEnum?,
+    val front_privacy: PkPrivacyEnum?,
+    val front_history_privacy: PkPrivacyEnum?,
+) {
+    constructor(privacy: PkPrivacyEnum) : this(
+        description_privacy = privacy,
+        pronoun_privacy = privacy,
+        member_list_privacy = privacy,
+        group_list_privacy = privacy,
+        front_privacy = privacy,
+        front_history_privacy = privacy,
+    )
+}
+
+@JvmRecord
+data class PkConfig(
+    val timezone: String? = null,
+    val pings_enabled: Boolean? = true,
+    val latch_timeout: Int? = null,
+    val member_default_private: Boolean? = true,
+    val group_default_private: Boolean? = true,
+    val show_private_info: Boolean? = false,
+    val member_limit: Int? = 1000,
+    val group_limit: Int? = 250,
+    val description_templates: List<String>? = null,
+)
+
+@JvmRecord
+data class PfSystemExtension(
+    val trust: Map<ULong, TrustLevel>?,
+    val autoType: AutoProxyMode?,
+    val autoProxy: String?,
+)
+
+@JvmRecord
+data class PfMemberExtension(
+    val age: String?,
+    val role: String?,
+)
+
+@Suppress("EnumEntryName")
+enum class PkPrivacyEnum {
+    public, private
 }
