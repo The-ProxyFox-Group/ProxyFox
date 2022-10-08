5b823deddb5f5c3d837d65cde4044484614a7298
Ampflower <ampflower@outlook.com>
2022-10-07 19:28:23 -0500

    Add UnexpectedValueProcessor for RecordAdapter

---
+++ modules/database/src/main/kotlin/dev/proxyfox/gson/UnexpectedValueProcessor.kt
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2022, The ProxyFox Group
+ *
+ * This Source Code is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package dev.proxyfox.gson
+
+import kotlin.reflect.KClass
+
+// Created 2022-01-10T03:36:52
+
+/**
+ * @author Ampflower
+ * @since ${version}
+ **/
+@Target(AnnotationTarget.FIELD, AnnotationTarget.CLASS, AnnotationTarget.PROPERTY, AnnotationTarget.VALUE_PARAMETER)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class UnexpectedValueProcessor<T>(
+    val value: KClass<out ValueProcessor<out T>>
+)
---
+++ modules/database/src/main/kotlin/dev/proxyfox/gson/ValueProcessor.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022, The ProxyFox Group
+ *
+ * This Source Code is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package dev.proxyfox.gson
+
+import com.google.gson.stream.JsonReader
+import dev.proxyfox.database.unsupported
+
+// Created 2022-01-10T03:41:13
+
+/**
+ * @author Ampflower
+ * @since ${version}
+ **/
+interface ValueProcessor<T> {
+    fun ifArray(reader: JsonReader): T = unsupported()
+    fun ifBoolean(reader: JsonReader): T = unsupported()
+    fun ifString(reader: JsonReader): T = unsupported()
+    fun ifNumber(reader: JsonReader): T = unsupported()
+}
