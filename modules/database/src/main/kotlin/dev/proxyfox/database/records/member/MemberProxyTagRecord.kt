cfe5628e6a3121b8265a371c1b8223a18e03bc75
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-10-11 03:28:05 -0500

    Refactor MemberProxyTagRecord handling in JsonDatabase

--- modules/database/src/main/kotlin/dev/proxyfox/database/records/member/MemberProxyTagRecord.kt
+++ modules/database/src/main/kotlin/dev/proxyfox/database/records/member/MemberProxyTagRecord.kt
@@ -8,6 +8,8 @@
 
 package dev.proxyfox.database.records.member
 
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 import org.bson.types.ObjectId
 
 // Created 2022-09-04T15:17:43
@@ -19,24 +21,32 @@ import org.bson.types.ObjectId
  **/
 class MemberProxyTagRecord {
     var _id: ObjectId = ObjectId()
+
+    // GSON-specific annotation for JSON database
+    @Expose(serialize = false, deserialize = false)
     var systemId: String = ""
+
+    // GSON-specific annotation for JSON database
+    @SerializedName(value = "memberId", alternate = ["member"])
     var memberId: String = ""
+
     var prefix: String? = null
     var suffix: String? = null
 
     constructor()
 
-    constructor(member: MemberRecord, action: MemberProxyTagRecord.() -> Unit) {
-        this.systemId = member.systemId
-        this.memberId = member.id
-        action()
-    }
-
     constructor(systemId: String, memberId: String, prefix: String?, suffix: String?) {
         this.systemId = systemId
         this.memberId = memberId
-        this.prefix = prefix
-        this.suffix = suffix
+        this.prefix = if (prefix == "") null else prefix
+        this.suffix = if (suffix == "") null else suffix
+    }
+
+    fun isEqual(other: MemberProxyTagRecord) = this === other || isEqual(prefix = other.prefix, suffix = other.suffix)
+
+    fun isEqual(prefix: String?, suffix: String?): Boolean {
+        return (this.prefix == prefix || (this.prefix.isNullOrEmpty() && prefix.isNullOrEmpty())) &&
+                (this.suffix == suffix || (this.suffix.isNullOrEmpty() && suffix.isNullOrEmpty()))
     }
 
     fun test(message: String): Boolean {
@@ -51,6 +61,23 @@ class MemberProxyTagRecord {
         val pLength = prefix?.length ?: 0
         val slength = suffix?.length ?: 0
         return message.substring(pLength, message.length - slength)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return this === other || (
+                other is MemberProxyTagRecord &&
+                        other.memberId == memberId &&
+                        other.systemId == systemId &&
+                        isEqual(other)
+                )
+    }
+
+    override fun hashCode(): Int {
+        var result = systemId.hashCode()
+        result = 31 * result + memberId.hashCode()
+        result = 31 * result + (prefix?.hashCode() ?: 0)
+        result = 31 * result + (suffix?.hashCode() ?: 0)
+        return result
     }
 
     override fun toString() = "${prefix ?: ""}text${suffix ?: ""}"
