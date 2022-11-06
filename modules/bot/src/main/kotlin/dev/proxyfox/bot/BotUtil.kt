50f26a7ce52bc09dc0b347745390b812e37f4a42
KJP12 <kjp12.k.joy.programer12@gmail.com>
2022-11-05 22:33:55 -0500

    Pass raw channel object for permission handling

--- modules/bot/src/main/kotlin/dev/proxyfox/bot/BotUtil.kt
+++ modules/bot/src/main/kotlin/dev/proxyfox/bot/BotUtil.kt
@@ -19,6 +19,7 @@ import dev.kord.core.behavior.channel.MessageChannelBehavior
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.builder.kord.KordBuilder
 import dev.kord.core.entity.Member
+import dev.kord.core.entity.channel.Channel
 import dev.kord.core.entity.channel.GuildMessageChannel
 import dev.kord.core.entity.channel.TextChannel
 import dev.kord.core.entity.channel.thread.ThreadChannel
@@ -320,10 +321,10 @@ data class Either<A, B>(
 val GuildMessageChannel.sendPermission
     get() = if (this is ThreadChannel) Permission.SendMessagesInThreads else Permission.SendMessages
 
-suspend fun GuildMessageChannel.permissionHolder() = if (this is ThreadChannel) kord.getChannelOf(parentId)!! else this
+suspend fun GuildMessageChannel.permissionHolder() = if (this is ThreadChannel) kord.getChannel(parentId)!! else this
 
 suspend fun MessageChannelBehavior.selfCanSend(): Boolean {
-    return if (this is GuildMessageChannel) permissionHolder().selfHasPermissions(Permissions(sendPermission, Permission.ViewChannel)) else true
+    return if (this is GuildMessageChannel) selfHasPermissions(Permissions(sendPermission, Permission.ViewChannel)) else true
 }
 
 suspend fun GuildMessageChannel.selfHasPermissions(permissions: Permissions): Boolean {
@@ -334,7 +335,7 @@ suspend fun GuildMessageChannel.selfHasPermissions(permission: Permission): Bool
     return permissionHolder().getEffectivePermissions(guild.getMember(kord.selfId)).contains(permission)
 }
 
-suspend fun GuildMessageChannel.getEffectivePermissions(member: Member): Permissions {
+suspend fun Channel.getEffectivePermissions(member: Member): Permissions {
     val map = data.permissionOverwrites.value.orEmpty().associateBy { it.id }
 
     val effective = EmptyBitSet()
