c00173f52def2f3fb9231ebec6279ff6fb5da09c
Oliver <59418903+Oliver-makes-code@users.noreply.github.com>
2022-11-06 09:44:57 -0600

    Remove unused class

--- modules/common/src/main/kotlin/dev/proxyfox/common/FoxFetch.kt
+++ modules/common/src/main/kotlin/dev/proxyfox/common/FoxFetch.kt
@@ -16,10 +16,6 @@ object FoxFetch {
     private val baseUrl = "https://api.tinyfox.dev"
     private val url = URL("https://api.tinyfox.dev/img?animal=fox&json")
 
-    private class FoxRecord {
-        var loc: String = ""
-    }
-
     suspend fun fetch()  = withContext(Dispatchers.IO) {
         baseUrl + url.openStream().reader().use { JsonParser.parseReader(it).asJsonObject["loc"].asString }
     }
