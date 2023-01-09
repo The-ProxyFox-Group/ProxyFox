1c5d45b614a7a03745fe9508fb6caabeade903cb
Ampflower <ampflower@outlook.com>
2023-01-08 22:57:24 -0600

    Convert ConversionMain.kt to Kotlin Serialization
    I honestly forgot this existed.

--- modules/conversion/src/main/kotlin/dev/proxyfox/conversion/ConversionMain.kt
+++ modules/conversion/src/main/kotlin/dev/proxyfox/conversion/ConversionMain.kt
@@ -8,21 +8,21 @@
 
 package dev.proxyfox.conversion
 
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonToken
 import dev.proxyfox.database.Database
 import dev.proxyfox.database.databaseFromString
-import dev.proxyfox.database.gson
-import dev.proxyfox.database.records.misc.ServerSettingsRecord
 import dev.proxyfox.database.etc.importer.PluralKitImporter
 import dev.proxyfox.database.etc.types.PkSystem
+import dev.proxyfox.database.records.misc.ServerSettingsRecord
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
 import org.slf4j.LoggerFactory
 import java.io.File
-import kotlin.io.path.Path
 import kotlin.system.exitProcess
 
 private val logger = LoggerFactory.getLogger("Converter")
 
+@OptIn(ExperimentalSerializationApi::class)
 suspend fun main(args: Array<String>) {
     var from: String? = null
     var to: String? = null
@@ -62,12 +62,8 @@ suspend fun main(args: Array<String>) {
             if (systems.exists()) {
                 logger.info("Importing systems.json...")
 
-                JsonReader(systems.reader()).use {
-                    val sysAdaptor = gson.getAdapter(PkSystem::class.java)
-                    it.beginObject()
-                    while (it.peek() != JsonToken.END_OBJECT) {
-                        val id = it.nextName().toULong()
-                        val obj = sysAdaptor.read(it) as PkSystem
+                systems.inputStream().use { input ->
+                    Json.decodeFromStream<Map<ULong, PkSystem>>(input).forEach { (id, obj) ->
                         try {
                             output.bulk {
                                 val pki = object : PluralKitImporter(directAllocation = true, ignoreUnfinished = true) {}
@@ -81,24 +77,19 @@ suspend fun main(args: Array<String>) {
                             throw Exception("Failed with $id -> $obj", e)
                         }
                     }
-                    it.endObject()
                 }
             }
 
             if (roles.exists()) {
                 logger.info("Importing roles.json...")
                 output.bulk {
-                    JsonReader(roles.reader()).use {
-                        it.beginObject()
-                        while (it.peek() != JsonToken.END_OBJECT) {
-                            val id = it.nextName().toULong()
-                            val role = it.nextLong().toULong()
+                    roles.inputStream().use { input ->
+                        Json.decodeFromStream<Map<ULong, ULong>>(input).forEach { (id, role) ->
                             createServerSettings(ServerSettingsRecord().apply {
                                 serverId = id
                                 proxyRole = role
                             })
                         }
-                        it.endObject()
                     }
                 }
             }
